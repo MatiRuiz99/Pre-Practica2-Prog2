2bb0f0709e5f82bec98ef380c98ac60ff3ef10db
Sebastian Tournier <sebastiantournier@gmail.com>
2022-08-23 13:38:41 -0300

    Intento de ej 1

--- PrePractica2_Ej_1.py
+++ PrePractica2_Ej_1.py
@@ -4,5 +4,25 @@
 
 #INICIO
 
+from traceback import print_tb
+
+i = 0
+mayor = 0
+lista = []
+lista.append(int(input("Ingrese un numero1: ")))
+lista.append(int(input("Ingrese un numero2: ")))
+lista.append(int(input("Ingrese un numero3: ")))
+maximo = max(lista)
+print(lista)
+print(maximo)
+
+for i in range(3):
+    if i == 0:
+        mayor = lista[i]
+    else:
+        if lista[i] > mayor:
+            mayor = lista[i]
+        
+print(mayor)
 
 #FIN
